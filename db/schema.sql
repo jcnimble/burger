0f39aba0e192320f26f6f8fbc08700fb68b8707d
jcnimble <jcginelli@outlook.com>
2019-05-07 00:03:31 -0400

    added key files with Cat Activity as a guide. Need to create ORM

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY (`id`)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured ) VALUES ('Original', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Smoke House', false);
+INSERT INTO burgers (burger_name,devoured ) VALUES ('Route 66', true);
+
+SELECT * FROM burgers;
