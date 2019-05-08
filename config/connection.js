33703e6a99958edf931325ce83401fa423660e27
jcnimble <jcginelli@outlook.com>
2019-05-08 11:18:31 -0400

    changed connection file for heroku upload

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+/* var connection = mysql.createConnection({
   host: process.env.MySQL_Database_host,
   port: process.env.PORT || 3306,
   user: "root",
   password: process.env.MySQL_Database_Password,
   database: "burgers_db"
-});
+}); */
 
+var connection;
+if (process.env.JAWSDB_URL) {
+   // Database is JawsDB on Heroku
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   // Database is local
+   connection = mysql.createConnection({
+       port: 3306,
+       host: process.env.MySQL_Database_host,
+       user: 'root',
+       password: process.env.MySQL_Database_Password,
+       database: "burgers_db"
+   })
+};
+ 
 // Make connection.
 connection.connect(function(err) {
   if (err) {
